941a901fe955b6ae5514b76ba9742352b7891178
mixxuss <mixxuss@gmail.com>
2017-01-13 12:18:03 +0300

    solve

--- password_strength.py
+++ password_strength.py
@@ -1,60 +1,48 @@
+import getpass
 
-'''
+
 def check_case_sens(passwd):
+    # Case sens checking
     if passwd == passwd.lower() or passwd == passwd.upper():
         return 0
-    else: return 1
+    return 2
 
 
 def check_num_dig(passwd):
+    # Digit contain checking
     if any(letter.isdigit() for letter in passwd):
-        return 1
+        return 2
     return 0
 
 
 def check_spec_char(passwd):
-    if any(letter in ('@','*','_','-','+','%','=','&','`','!','$') for letter in passwd):
-        return 1
+    # Special characters cheking
+    if any(letter in ('$&+,:;=?@#|\'<>.-^*()%!') for letter in passwd):
+        return 2
     return 0
 
 
 def check_blacklist(passwd):
-    if passwd.lower() in ('password', 'mypass', 'ny2017', 'privet', '12345', '123456'):
+    # Blacklist checking
+    if passwd.lower() in open('top100_pass.txt').read():
         return 0
-    return 1
+    return 2
 
 
-def check_user_info(name, passwd):
-    if passwd.lower() in list(name):
+def check_user_info(username, passwd):
+    # Username match checking
+    if passwd.lower() in username.lower():
         return 0
-    return 1
+    return 2
 
 
-def check_comp_name(passwd):
-    pass
+def get_password_strength(password):
+    # Execute other functions and return strength score
+    result = check_case_sens(password) + check_num_dig(password) + check_spec_char(password) + check_user_info(user, password)* check_blacklist(password)
+    return result
 
-
-def check_match_num(passwd):
-    pass
-'''
-import zxcvbn
-
-
-
-def get_password_strength(password, first_name, last_name):
-    results = zxcvbn.zxcvbn(password, user_inputs=[first_name, last_name])
-    print(results)
 
 if __name__ == '__main__':
-    first_name = 'Alex'
-    last_name = 'Foley'
-    password = 'AlexFoley123'
-    get_password_strength(password, first_name, last_name)
-
-
-    '''first_name = 'Alex'
-    last_name = 'Parker'
-    passwd = '123456'
-    user_name = first_name + last_name
-    print(check_user_info(user_name, passwd))
-'''
+    user = input('Username: ')
+    password = getpass.getpass('Password: ')
+    print('Your password score is:', get_password_strength(password))
