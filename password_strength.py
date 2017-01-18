263a9f72b72df0ea2332f1224ecf9c8c6ef52da4
mixxuss <mixxuss@gmail.com>
2017-01-18 17:16:11 +0300

    Added: optional arg with blacklist file               PEP8 accord, filepath check

--- password_strength.py
+++ password_strength.py
@@ -1,48 +1,51 @@
-import getpass
+import getpass, os, argparse
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-b', help='Path to passwords blacklist file')
+    return parser
 
 
 def check_case_sens(passwd):
-    # Case sens checking
-    if passwd == passwd.lower() or passwd == passwd.upper():
-        return 0
-    return 2
+    return yes_or_no(passwd != passwd.lower() or passwd != passwd.upper())
 
 
 def check_num_dig(passwd):
-    # Digit contain checking
-    if any(letter.isdigit() for letter in passwd):
-        return 2
-    return 0
+    return yes_or_no(any(letter.isdigit() for letter in passwd))
 
 
 def check_spec_char(passwd):
-    # Special characters cheking
-    if any(letter in ('$&+,:;=?@#|\'<>.-^*()%!') for letter in passwd):
+    return yes_or_no(any(letter in ('$&+,:;=?@#|\'<>.-^*()%!') for letter in passwd))
+
+
+def check_blacklist(passwd, blacklist_file):
+    if not os.path.exists(blacklist_file):
         return 2
-    return 0
-
-
-def check_blacklist(passwd):
-    # Blacklist checking
-    if passwd.lower() in open('top100_pass.txt').read():
-        return 0
-    return 2
+    return yes_or_no(passwd.lower() not in open(blacklist_file).read())
 
 
 def check_user_info(username, passwd):
-    # Username match checking
-    if passwd.lower() in username.lower():
-        return 0
-    return 2
+    return yes_or_no(passwd.lower() not in username.lower())
 
 
-def get_password_strength(password):
-    # Execute other functions and return strength score
-    result = check_case_sens(password) + check_num_dig(password) + check_spec_char(password) + check_user_info(user, password)* check_blacklist(password)
+def yes_or_no(boolean):
+    return boolean * 2
+
+
+def get_password_strength(password, path):
+    result = check_case_sens(password) + check_num_dig(password) + check_spec_char(password) + \
+             check_user_info(user, password) * check_blacklist(password, path)
     return result
 
 
 if __name__ == '__main__':
+    parsed_args = create_parser()
+    args = parsed_args.parse_args()
+    if args.b:
+        path = args.b
+    else:
+        path = ''
     user = input('Username: ')
     password = getpass.getpass('Password: ')
-    print('Your password score is:', get_password_strength(password))
+    print('Your password score is', get_password_strength(password, path))
