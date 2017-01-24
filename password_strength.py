e253c11a136725d12af43430e8d488cc7ed6233d
mixxuss <mixxuss@gmail.com>
2017-01-24 17:21:22 +0300

    Deleted unnesessary comments Fixed brainfuck func

--- password_strength.py
+++ password_strength.py
@@ -8,37 +8,37 @@ def create_parser():
 
 
 def check_case_sens(passwd):
-    return yes_or_no(passwd != passwd.lower() or passwd != passwd.upper())
+    return check_condition(passwd != passwd.lower() or passwd != passwd.upper())
 
 
 def check_num_dig(passwd):
-    return yes_or_no(any(letter.isdigit() for letter in passwd))
+    return check_condition(any(letter.isdigit() for letter in passwd))
 
 
 def check_spec_char(passwd):
-    return yes_or_no(any(letter in ('$&+,:;=?@#|\'<>.-^*()%!') for letter in passwd))
+    return check_condition(any(letter in ('$&+,:;=?@#|\'<>.-^*()%!') for letter in passwd))
 
 
 def check_blacklist(passwd, blacklist_file):
     if not os.path.exists(blacklist_file):
         return 2
-    return yes_or_no(passwd.lower() not in open(blacklist_file).read())
+    return check_condition(passwd.lower() not in open(blacklist_file).read())
 
 
 def check_user_info(username, passwd):
-    return yes_or_no(passwd.lower() not in username.lower())
+    return check_condition(passwd.lower() not in username.lower())
 
 
-def yes_or_no(boolean):
-    # Для проверяющего: если функция получает False - то возврщает 0, если получает True - 2
-    return boolean * 2
+def check_condition(boolean):
+    if boolean:
+        return 2
+    else:
+        return 0
 
 
 def get_password_strength(password, path):
-    # Для проверяющего: если мы находим пароль в блеклисте - то оцениваем его в 0, для этого и умножаем на результат
-    # функции check_blacklist
     result = check_case_sens(password) + check_num_dig(password) + check_spec_char(password) + \
-             check_user_info(user, password) * check_blacklist(password, path)
+             check_user_info(user, password) + check_blacklist(password, path)
     return result
 
 
